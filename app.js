9ec48c3d633fc00892c33209a3ca173ed60a6e73
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-08-10 15:16:56 -0500

    After Davie

--- app.js
+++ app.js
@@ -2,56 +2,49 @@ console.log('connected')
 
 document.addEventListener('DOMContentLoaded', (ev) => {
 
-//make 2x2 canvas of squares
-let canvasDivCount = 0;
-while (canvasDivCount < 216) {
-    const canvasDiv = document.createElement('div')
-    // canvasDiv.setAttribute('class','canvasDiv')
-    document.body.appendChild(canvasDiv)
-    //add event listener to the squares so when clicked, they turn red
-    canvasDiv.addEventListener('click', (ev) => {
-        const colorId = document.getElementById('currentColor').classList
-        canvasDiv.setAttribute('class',colorId);
-    })
-    canvasDivCount ++
+    //make 2x2 canvas of squares
+    let canvasDivCount = 0;
+    let x;
+    while (canvasDivCount < 216) {
+        const canvasDiv = document.createElement('div');
+        // canvasDiv.setAttribute('class','canvasDiv')
+        document.body.appendChild(canvasDiv);
+        //add event listener to the squares so when clicked, they turn red
 
-}
+        //messing with mouse functions
 
-//create current color indicator square
-let currentColor = document.createElement('div')
-document.body.appendChild(currentColor);
-currentColor.style.float = 'center'
-currentColor.style.margin = '10px'
-currentColor.style.width = '95%'
-currentColor.style.paddingBottom = '1%'
-currentColor.classList.add('palDivClass')
-currentColor.innerText = 'Current Color'
-currentColor.id= 'currentColor'
+        canvasDiv.addEventListener(`click`, (ev) => {
+            // console.log(ev.type);
+            const colorId = document.getElementById('currentColor').classList;
+            canvasDiv.setAttribute('class',colorId);
+        });
 
-//create color palette
-let colorList = ['red', 'blue', 'aqua', 'grey','maroon','fuchsia','green','purple','lime','yellow','orange','cornflowerblue']
-let palBoxSize = 100/colorList.length
-for (let k = 0; k < colorList.length; k++) {
-    const palDiv = document.createElement('div');
-    palDiv.style.width = '10%'
-    document.body.appendChild(palDiv);
-    palDiv.classList.add('palDivClass',colorList[k])
-    palDiv.addEventListener('click', (ev) => {
-        //change current div class to whatever class this one is.
-        currentColor.setAttribute('class',colorList[k])
-        
-    })
+        canvasDivCount ++;
+    }
 
-}
+    //create current color indicator square
+    let currentColor = document.createElement('div')
+    document.body.appendChild(currentColor);
+    currentColor.style.float = 'center';
+    currentColor.style.margin = '10px';
+    currentColor.style.width = '95%';
+    currentColor.style.paddingBottom = '1%';
+    currentColor.classList.add('palDivClass');
+    currentColor.innerText = 'Current Color';
+    currentColor.id= 'currentColor';
 
-
-
-
-
-
-
-
-
-
-
+    //create color palette
+    let colorList = ['red', 'blue', 'aqua', 'grey','maroon','fuchsia','green','purple','lime','yellow','orange','cornflowerblue'];
+    let palBoxSize = 100/colorList.length;
+    for (let k = 0; k < colorList.length; k++) {
+        const palDiv = document.createElement('div');
+        palDiv.style.width = '10%';
+        document.body.appendChild(palDiv);
+        palDiv.classList.add('palDivClass',colorList[k]);
+        palDiv.addEventListener('click', (ev) => {
+            //change current div class to whatever class this one is.
+            currentColor.setAttribute('class',colorList[k]);
+            
+        });
+    };
 })
